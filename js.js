89af3d4a81c211c0d71698eb445e632a8278e02b
Verezub Olena <ussanel@ukr.net>
2023-03-08 22:56:03 +0200

    finish

--- js.js
+++ js.js
@@ -1,6 +1,9 @@
 let text = 'You can ignore single and double quotes like this: \\\'\\\' \\\"\\\"'
 console.log(text);
 
+
+
+
 alert('Please enter your data');
 let userName = prompt('Enter your name:');
 alert(`Your name is: ${userName}`);
@@ -13,3 +16,16 @@ alert(`Your full name is: ${userName} ${userSurname}, you age: ${age}`);
 
 let admin = confirm('Are you admin?');
 alert(`Your full name is: ${userName} ${userSurname}, you age: ${age}, admin status: ${admin}`);
+
+
+onsole.log (Boolean (alert('5'))); 
+// Діалогове вікно з текстом "5" і кнопкою Ок - виконуеться alert. В консолі false - виконується boolean,так як немає alert не має типу 
+console.log (true>false);
+// В порівнянні true= 1, false=0. Одже 1>0 відповідно. В консолі true -порівняння вірне
+console.log (true > 5 );
+// В порівнянні true= 1, 5 - цифра в даному випадку.Одже 1>5 відповідно. В консолі false - порівняння невірне
+console.log (true <'5');
+// В порівнянні true= 1, 5 - string в даному випадку перетворюється в number.Одже 1<5 відповідно . В консолі true -порівняння вірне
+console.log (true > null);
+// / В порівнянні true= 1, null=0. Одже 1>0 відповідно. В консолі true -порівняння вірне
+
